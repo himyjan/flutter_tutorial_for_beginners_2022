c1e0509c63a4c2bb02af8d81e30a69202f8b711c
himyjan <51815522+himyjan@users.noreply.github.com>
2022-11-21 17:49:22 +0800

    upload code

---
+++ lib/pages/register_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+    );
+  }
+}
